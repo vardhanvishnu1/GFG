be170b0fe906fd1943d8198e293782cd743a8c9f
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-02-05 22:48:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -7,26 +7,30 @@ class Solution {
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
         }
-        vector<int>visited(V,false);
         vector<int>parent(V,-1);
+        vector<bool>visited(V,false);
         for(int i=0;i<V;i++){
             if(!visited[i]){
                 queue<int>q;
                 q.push(i);
                 visited[i] = true;
-                while(!q.empty()){
-                    int curr_node = q.front();q.pop();
-                    for(auto neigh : adj[curr_node]){
-                        if(!visited[neigh]){
-                            visited[neigh] = true;
-                            parent[neigh] = curr_node;
-                            q.push(neigh);
+                    while(!q.empty()){
+                            int curr = q.front();
+                            q.pop();
+                            for(auto neigh : adj[curr]){
+                                if(!visited[neigh]){
+                                    visited[neigh] = true;
+                                    parent[neigh] = curr;
+                                    q.push(neigh);
+                                }
+                                else{
+                                    if(parent[curr]!=neigh){
+                                        return true;
+                                    }
+                                }
+                            }
                         }
-                        else{
-                            if(parent[curr_node]!=neigh) return true;
-                        }
-                    }
-                }
+                    
             }
         }
         return false;
