616004837116129acc9cc5b3ac7a846b0cd0d4c3
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-03-19 01:20:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -0,0 +1,46 @@
+class Solution {
+  public:
+  int lower(vector<int>& nums, int target){
+    int n =nums.size();
+        int  lo=0;
+        int hi =n-1;
+        int st = -1;
+        int mid;
+        while(lo<=hi){
+            mid = lo + (hi-lo)/2;
+            if(nums[mid]==target){
+                st = mid;hi=mid-1;
+            }
+            else if(nums[mid]>target) hi = mid-1;
+            else lo = mid+1;
+        }
+        return st;
+}
+
+int upper(vector<int>& nums, int target){
+    int n =nums.size();
+        int  lo=0;
+        int hi =n-1;
+        int end = -1;
+        int mid;
+        while(lo<=hi){
+            mid = lo + (hi-lo)/2;
+            if(nums[mid]==target){
+                end = mid;lo = mid+1;
+            }
+            else if(nums[mid]>target) hi = mid-1;
+            else lo = mid+1;
+        }
+        return end;
+}
+    int countFreq(vector<int>& nums, int target) {
+        // code here
+        int n =nums.size();
+        int  lo=0;
+        int hi =n-1;
+        int st=-1,end=-1;
+        st = lower(nums,target);end = upper(nums,target);
+        if(end==-1||st==-1) return 0;
+        return end-st+1;
+    }
+};
