e5a9bad31afc3b7a1437ca986ce3c7c03437076b
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-05-31 15:42:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
@@ -1,37 +1,25 @@
 class Solution {
   public:
     double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
-        int n = val.size();
-        // Use double for the ratio to avoid integer division truncation
-        vector<pair<double, int>> v; 
-        
-        for(int i = 0; i < n; i++) {
-            // Force double division by casting
-            double ratio = (double)val[i] / wt[i];
-            v.push_back({ratio, i});
+        // code here
+        priority_queue<pair<double,int>>pq;
+        for(int i=0;i<val.size();i++){
+            double x = (val[i]*1.0)/(wt[i]*1.0);
+            pq.push({x,wt[i]});
         }
-        
-        // Sort by ratio (ascending by default)
-        sort(v.begin(), v.end());
-        
-        double ans = 0;
-        double remainingCapacity = (double)capacity;
-
-        // Iterate backwards from the highest ratio
-        for(int i = n - 1; i >= 0 && remainingCapacity > 0; i--) {
-            int idx = v[i].second; // Get the original index of the item
-            
-            if(wt[idx] <= remainingCapacity) {
-                // Take the whole item
-                remainingCapacity -= wt[idx];
-                ans += val[idx];
-            } else {
-                // Take the fraction that fits
-                ans += v[i].first * remainingCapacity;
-                remainingCapacity = 0;
+        double ans =0;
+        while(capacity>0&&!pq.empty()){
+            auto [curr,weight] = pq.top();
+            pq.pop();
+            if(capacity>=weight){
+                capacity-=weight;
+                ans+=curr*weight;
+            }
+            else{
+                ans+=capacity*curr;
+                capacity=0;
             }
         }
-        
         return ans;
     }
 };
