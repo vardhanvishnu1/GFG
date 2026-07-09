9bf4419ec5f6fb50d3d7ed3f62c81091aced3832
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-07-09 21:12:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.cpp
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.cpp
@@ -2,32 +2,16 @@ class Solution {
   public:
     long subarrayXor(vector<int> &arr, int k) {
         // code here
-          long count = 0;
-    int current_xor = 0;
-    
-    // map to store the frequency of prefix XORs
-    unordered_map<int, int> freq;
-    
-    // Base case: a prefix XOR of 0 has occurred once
-    freq[0] = 1;
-    
-    for (int num : arr) {
-        // Update prefix XOR for current element
-        current_xor ^= num;
-        
-        // We need: current_xor ^ target = k
-        // Which means: target = current_xor ^ k
-        int target = current_xor ^ k;
-        
-        // If target exists in map, add its frequency to count
-        if (freq.find(target) != freq.end()) {
-            count += freq[target];
+        int n = arr.size();
+        long ans = 0;
+        map<int,int>freq;
+        freq[0] = 1;
+        int cur = 0;
+        for(int i=0;i<n;i++){
+            cur^=arr[i];
+            if(freq.count(k^cur)) ans+=freq[k^cur];
+            freq[cur]++;
         }
-        
-        // Update the frequency of the current prefix XOR
-        freq[current_xor]++;
-    }
-    
-    return count;
+        return ans;
     }
 };
